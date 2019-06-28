4d434088f82c072963705f961286212033b5e296
Yu Le <co.nullne@gmail.com>
2019-06-28 14:11:46 +0800

    new path trie

---
+++ cmd/volume/path-trie.go
@@ -0,0 +1,80 @@
+package volume
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type pathTrie struct {
+	sync.RWMutex
+	count    *int32
+	children map[string]*pathTrie
+}
+
+func newPathTrie() *pathTrie {
+	return &pathTrie{
+		children: make(map[string]*pathTrie),
+		count:    new(int32),
+	}
+}
+
+func (t *pathTrie) getChild(key string) (*pathTrie, bool) {
+	t.RLock()
+	defer t.RUnlock()
+	child, ok := t.children[key]
+	return child, ok
+}
+
+func (t *pathTrie) list(segments []string) []string {
+	node := t
+	for _, s := range segments {
+		child, ok := node.getChild(s)
+		if !ok {
+			return nil
+		}
+		node = child
+	}
+
+	node.RLock()
+	entries := make([]string, 0, len(node.children))
+	for k := range node.children {
+		entries = append(entries, k)
+	}
+	node.RUnlock()
+	return entries
+}
+
+// recursive may be better
+func (t *pathTrie) delete(segments []string) {
+	node := t
+	for _, s := range segments {
+		child, ok := node.getChild(s)
+		if !ok {
+			return
+		}
+		if res := atomic.AddInt32(child.count, -1); res > 0 {
+			node = child
+			continue
+		}
+		// delete the node
+		node.Lock()
+		delete(node.children, s)
+		node.Unlock()
+		return
+	}
+}
+
+func (t *pathTrie) put(segments []string) {
+	node := t
+	for _, s := range segments {
+		child, ok := node.getChild(s)
+		if !ok {
+			child = newPathTrie()
+			node.Lock()
+			node.children[s] = child
+			node.Unlock()
+		}
+		atomic.AddInt32(child.count, 1)
+		node = child
+	}
+}
---
+++ cmd/volume/path-trie_test.go
@@ -0,0 +1,63 @@
+package volume
+
+import (
+	"sort"
+	"strings"
+	"testing"
+)
+
+func TestPathTrie(t *testing.T) {
+	segment := func(s string) []string {
+		if s == "" {
+			return nil
+		}
+		return strings.Split(s, "/")
+	}
+
+	root := newPathTrie()
+
+	cases := []struct {
+		fn       string
+		path     string
+		expected []string
+	}{
+		{"list", "", []string{}},
+		{"list", "a/b", []string{}},
+		{"delete", "", []string{}},
+		{"delete", "a/b", []string{}},
+		{"put", "a/b", []string{}},
+		{"list", "", []string{"a"}},
+		{"list", "a", []string{"b"}},
+		{"list", "a/b", []string{}},
+		{"put", "a/c", []string{}},
+		{"list", "a", []string{"b", "c"}},
+		{"list", "", []string{"a"}},
+		{"delete", "a/b", []string{}},
+		{"list", "a", []string{"c"}},
+		{"delete", "a/c", []string{}},
+		{"list", "a", []string{}},
+		{"list", "", []string{}},
+	}
+
+	for _, tc := range cases {
+		switch tc.fn {
+		case "list":
+			ls := root.list(segment(tc.path))
+			sort.Strings(ls)
+			if len(ls) != len(tc.expected) {
+				t.Errorf("got %v, wanna %v", ls, tc.expected)
+				continue
+			}
+			for i, v := range ls {
+				if v != tc.expected[i] {
+					t.Errorf("got %v, wanna %v", ls, tc.expected)
+					break
+				}
+			}
+		case "put":
+			root.put(segment(tc.path))
+		case "delete":
+			root.delete(segment(tc.path))
+		}
+	}
+}
