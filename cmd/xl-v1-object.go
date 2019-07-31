ccb9172539b6ac5106c5c1f2e2fd752079d8dc54
Yu Le <co.nullne@gmail.com>
2019-07-31 18:40:25 +0800

    comment read lock

--- cmd/xl-v1-object.go
+++ cmd/xl-v1-object.go
@@ -159,19 +159,21 @@ func (xl xlObjects) GetObjectNInfo(ctx context.Context, bucket, object string, r
 	var nsUnlocker = func() {}
 
 	// Acquire lock
-	if lockType != noLock {
-		lock := xl.nsMutex.NewNSLock(bucket, object)
-		switch lockType {
-		case writeLock:
-			if err = lock.GetLock(globalObjectTimeout); err != nil {
-				return nil, err
+	if isMinioMetaBucketName(bucket) {
+		if lockType != noLock {
+			lock := xl.nsMutex.NewNSLock(bucket, object)
+			switch lockType {
+			case writeLock:
+				if err = lock.GetLock(globalObjectTimeout); err != nil {
+					return nil, err
+				}
+				nsUnlocker = lock.Unlock
+			case readLock:
+				if err = lock.GetRLock(globalObjectTimeout); err != nil {
+					return nil, err
+				}
+				nsUnlocker = lock.RUnlock
 			}
-			nsUnlocker = lock.Unlock
-		case readLock:
-			if err = lock.GetRLock(globalObjectTimeout); err != nil {
-				return nil, err
-			}
-			nsUnlocker = lock.RUnlock
 		}
 	}
 
@@ -227,11 +229,13 @@ func (xl xlObjects) GetObjectNInfo(ctx context.Context, bucket, object string, r
 // length indicates the total length of the object.
 func (xl xlObjects) GetObject(ctx context.Context, bucket, object string, startOffset int64, length int64, writer io.Writer, etag string, opts ObjectOptions) error {
 	// Lock the object before reading.
-	objectLock := xl.nsMutex.NewNSLock(bucket, object)
-	if err := objectLock.GetRLock(globalObjectTimeout); err != nil {
-		return err
+	if isMinioMetaBucketName(bucket) {
+		objectLock := xl.nsMutex.NewNSLock(bucket, object)
+		if err := objectLock.GetRLock(globalObjectTimeout); err != nil {
+			return err
+		}
+		defer objectLock.RUnlock()
 	}
-	defer objectLock.RUnlock()
 	return xl.getObject(ctx, bucket, object, startOffset, length, writer, etag, opts)
 }
 
@@ -410,11 +414,13 @@ func (xl xlObjects) getObjectInfoDir(ctx context.Context, bucket, object string)
 // GetObjectInfo - reads object metadata and replies back ObjectInfo.
 func (xl xlObjects) GetObjectInfo(ctx context.Context, bucket, object string, opts ObjectOptions) (oi ObjectInfo, e error) {
 	// Lock the object before reading.
-	objectLock := xl.nsMutex.NewNSLock(bucket, object)
-	if err := objectLock.GetRLock(globalObjectTimeout); err != nil {
-		return oi, err
+	if isMinioMetaBucketName(bucket) {
+		objectLock := xl.nsMutex.NewNSLock(bucket, object)
+		if err := objectLock.GetRLock(globalObjectTimeout); err != nil {
+			return oi, err
+		}
+		defer objectLock.RUnlock()
 	}
-	defer objectLock.RUnlock()
 
 	if err := checkGetObjArgs(ctx, bucket, object); err != nil {
 		return oi, err
